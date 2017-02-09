2ebf8080673c21e1c6d10f0ca0074a8bd7716c0f
Luca Marchesini <luca@kuzzle.io>
2017-02-09 12:10:50 +0100

    Formatting

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ set -e
 
 # list of colors
 BOLD=$(tput bold)
-# NORMAL=$(tput sgr0)
 RED="\\033[1;31m"
 BLUE="\\033[1;34m"
 GREEN="\\033[1;32m"
+YELLOW="\\033[1;33m"
 NORMAL="\\033[0;39m"
 RT="\r\n"
 
@@ -22,6 +22,10 @@ writeBold() {
   echo -e "${BOLD} $1$2" "$NORMAL" >&2
 }
 
+promptBold() {
+  echo -n -e "${BOLD} $1$2" "$NORMAL" >&2
+}
+
 commandExists() {
   command -v "$@" > /dev/null 2>&1
 }
@@ -29,15 +33,14 @@ commandExists() {
 checkRoot() {
   if [ "$EUID" -ne 0 ]; then
     echo
-    writeBold "This script needs to be executed with root privileges."
+    writeBold "$YELLOW" "[✖] This script needs to be executed with root privileges."
     echo
-    exit
+    exit 1
   fi
 }
 
 findOSType()
 {
-  writeBold "Determining os type..."
   osType=$(uname)
   if [ "$osType" == "Linux" ]; then
     if [ -f /etc/os-release ]; then
@@ -51,34 +54,44 @@ findOSType()
 
 isOSSupported()
 {
+  echo
   # echo "Detected OS is" $CURRENT_OS
   case "$CURRENT_OS" in
     "UBUNTU" | "DEBIAN")
     {
-      write "$GREEN" "$CURRENT_OS is well supported."
+      write "$GREEN" "[✔] Your OS ($CURRENT_OS) is officially supported."
       OS_IS_SUPPORTED=1
     } ;;
     *)
     {
-      echo "$CURRENT_OS is not supported."
+      write "$YELLOW" "[✖] Your OS ($CURRENT_OS) is not officially supported."
+      write           "    This means we didn't thoroughly test Kuzzle on your"
+      write           "    system. It is likely to work, so you may want to continue."
       OS_IS_SUPPORTED=0
       while [[ "$proceedNotSupported" != [yYnN] ]]
       do
-        echo -n "Do you want to continue anyway? (y/N) "
+        promptBold "[❓] Do you want to continue? (y/N)"
         read proceedNotSupported trash
         case "$proceedNotSupported" in
           [yY])
             echo
-            echo "Ok. We encourage you to send us feedback at tech@kuzzle.io"
+            writeBold "$GREEN" "Great! Join us on Gitter if you need help (https://gitter.im/kuzzleio/kuzzle)"
             ;;
           [nN] | '')
             echo
-            echo "Aborting."
-            exit 1
+            writeBold "$BLUE" "Ok. We encourage you to get in touch with the team (tech@kuzzle.io)"
+            writeBold "$BLUE" "to request support for your system."
+            echo
+            if commandExists curl; then
+              # TODO send feedback to analytics system to notify that an install
+              # has been attempted on a non-supported system
+              echo LOL
+            fi
+            exit 2
             ;;
           *)
             echo
-            echo "I did not understand your answer."
+            writeBold "$RED" "[✖] I did not understand your answer."
             ;;
         esac
       done
@@ -88,93 +101,80 @@ isOSSupported()
 
 installDocker() {
   echo
-  echo "This script needs Docker, but it does not seem to be installed."
-  echo "This script can install Docker for you, otherwise you can do it manually"
-  echo "(you can find exhaustive instructions at https://docs.docker.com/engine/installation/"
-  echo
+  writeBold "[ℹ] Docker must be installed to run Kuzzle."
+  writeBold "    This script can install Docker for you, otherwise you can do it manually."
+  write     "    More information at https://docs.docker.com/engine/installation/"
   while [[ "$installDocker" != [yYnN] ]]
   do
-    echo -n "Do you want to install Docker now? (y/N) "
+    promptBold "[❓] Do you want to install Docker now? (y/N)"
     read installDocker trash
     case $installDocker in
       [yY])
         echo
         if commandExists curl; then
-          echo "Installing Docker..."
+          writeBold "[ℹ] Installing Docker..."
           curl -sSL https://get.docker.com/ | sh
+          echo
+          writeBold "$GREEN" "[✔] Docker successfully installed."
         else
-          echo "This script needs curl to launch the Docker installation script,"
-          echo "but it does not seem to be installed on your system."
-          echo "Please install it and re-run this script."
-          exit 1
+          writeBold "$RED" "[✖] curl needs to be installed to launch the Docker installation script,"
+          writeBold "$RED" "    but it does not seem to be installed on your system."
+          echo
+          writeBold "$BLUE" "Please install curl and re-run this script."
+          echo
+          exit 3
         fi
         ;;
       [nN] | '')
         echo
-        echo "Aborting. "
-        exit 1
+        writeBold "$BLUE" "Ok. Please install Docker and re-run this script. "
+        echo
+        exit 0
         ;;
       *)
         echo
-        echo "I did not understand your answer."
+        writeBold "$RED" "[✖] I did not understand your answer."
         ;;
-    esac
-  done
-}
-
-runDocker() {
-  echo
-  echo "Docker does not seem to be running on your system. This script can start it for you,"
-  echo "or you can do it manually."
-  echo
-  while [[ "$runDocker" != [yYnN] ]]
-  do
-    echo -n "Do you want to run Docker now? (y/N) "
-    read  runDocker trash
-    case "$runDocker" in
-      [yY])
-        ;;
-      [nN] | '')
-        ;;
-      *)
-      echo
-      echo "I did not understand your answer."
-      ;;
     esac
   done
 }
 
 installDockerCompose() {
   echo
-  echo "This script needs Docker Compose, but it does not seem to be installed."
-  echo "This script can install Docker Compose for you, otherwise you can do it manually"
-  echo "(you can find exhaustive instructions at https://docs.docker.com/compose/install/"
-  echo
+  writeBold "[ℹ] Docker Compose must be installed to run Kuzzle."
+  writeBold "    This script can install Docker Compose for you, otherwise you can do it manually."
+  write     "    More information at https://docs.docker.com/compose/install/"
   while [[ "$installDockerCompose" != [yYnN] ]]
   do
-    echo -n "Do you want to install Docker Compose now? (y/N) "
+    promptBold "[❓] Do you want to install Docker Compose now? (y/N)"
     read installDockerCompose trash
     case "$installDockerCompose" in
       [yY])
         if commandExists curl; then
-          echo "Installing Docker Compose..."
+          echo
+          writeBold "Installing Docker Compose..."
           curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
           chmod +x /usr/local/bin/docker-compose
+          echo
+          writeBold "$GREEN" "[✔] Docker Compose successfully installed."
         else
-          echo "This script needs curl to launch the Docker Compose installation script,"
-          echo "but it does not seem to be installed on your system."
-          echo "Please install it and re-run this script."
-          exit -1
+          writeBold "$RED" "[✖] curl needs to be installed to launch the Docker Compose installation script,"
+          writeBold "$RED" "    but it does not seem to be installed on your system."
+          echo
+          writeBold "$BLUE" "Please install curl and re-run this script."
+          echo
+          exit 3
         fi
         ;;
       [nN] | '')
         echo
-        echo "Aborting. "
-        exit -1
+        writeBold "$BLUE" "Ok. Please install Docker Compose and re-run this script."
+        echo
+        exit 0
         ;;
       *)
         echo
-        echo "I did not understand your answer."
+        writeBold "$RED" "[✖] I did not understand your answer."
         ;;
     esac
   done
@@ -187,30 +187,36 @@ setupMapCount() {
 
   if [ -z "$MAP_COUNT" ] || [ $MAP_COUNT -lt $REQUIRED_MAP_COUNT ]; then
     echo
-    echo "Kuzzle needs the kernel configuration variable vm.max_map_count to be set to at least $REQUIRED_MAP_COUNT,"
-    echo "but it seems to be set to $MAP_COUNT on your system."
-    echo "This script can set it automatically or you can do it manually."
-    echo
+    writeBold "[ℹ] The kernel configuration variable vm.max_map_count must be set to at least $REQUIRED_MAP_COUNT"
+    writeBold "    for Kuzzle to work properly, but it seems to be set to $MAP_COUNT on your system."
+    writeBold "    This script can set it automatically or you can do it manually."
+    write     "    More information at https://www.elastic.co/guide/en/elasticsearch/reference/5.x/vm-max-map-count.html"
     while [[ "$setVmParam" != [yYnN] ]]
     do
-      echo -n "Do you want to set the vm.max_map_count now? (y/N) "
+      promptBold "[❓] Do you want to set the vm.max_map_count now? (y/N) "
       read setVmParam trash
       case "$setVmParam" in
         [yY])
+          echo
+          writeBold "Setting kernel variable vm.max_map_count to $REQUIRED_MAP_COUNT..."
           sysctl -w vm.max_map_count=$REQUIRED_MAP_COUNT
           if [ -z "$MAP_COUNT" ]; then
             echo "vm.max_map_count=$REQUIRED_MAP_COUNT" > $SYSCTL_CONF_FILE
           else
             sed 's/vm.max_map_count=.+/vm.max_map_count=$REQUIRED_MAP_COUNT/g' $SYSCTL_CONF_FILE > $SYSCTL_CONF_FILE
           fi
+          echo
+          writeBold "$GREEN" "[✔] Kernel variable successfully set."
           ;;
         [nN] | '')
           echo
-          echo "Aborting."
+          writeBold "$BLUE" "Ok. Please set the kernel variable and re-run this script."
+          echo
+          exit 0
           ;;
         *)
         echo
-        echo "I did not understand your answer."
+        writeBold "$RED" "[✖] I did not understand your answer."
         ;;
       esac
     done
@@ -219,65 +225,86 @@ setupMapCount() {
 
 collectPersonalData() {
   echo
-  writeBold "We'd be happy to know a little bit about you."
-  echo
-  write "What's your email address? (press Enter to skip)"
+  writeBold  "[ℹ] Please let us know a little bit about youself."
+  promptBold "    What's your email address?${NORMAL} (press Enter to skip)"
   read email trash
-  write "What's your name? (press Enter to skip)"
+  promptBold "    What's your name?${NORMAL} (press Enter to skip)"
   read firstName lastName otherName yetAnotherName trash
-  write "What do you plan to use Kuzzle for? (press Enter to skip)"
+  promptBold "    What do you plan to use Kuzzle for?${NORMAL} (press Enter to skip)"
   read purpose
   # TODO send collected data to analytics service
 }
 
 startKuzzle() {
   echo
-  echo "Starting Kuzzle..."
+  writeBold "Downloading Kuzzle launch file..."
   echo
   composerYMLURL="https://raw.githubusercontent.com/kuzzleio/kuzzle-build/master/docker-compose/kuzzle-docker-compose.yml"
   composerYMLPath="kuzzle-docker-compose.yml"
   curl -XGET $composerYMLURL > $composerYMLPath
   echo
-  write "$GREEN" "The docker-compose YML file has been successfully downoloaded!"
+  writeBold "$GREEN" "[✔] The Kuzzle launch file has been successfully downloaded."
+  writeBold          "    This script can launch Kuzzle automatically or you can do it"
+  writeBold          "    manyally using Docker Compose."
+  write              "    To manyally launch Kuzzle you can type the following command:"
+  write              "    docker-compose -f $composerYMLPath up"
   while [[ "$launchTheStack" != [yYnN] ]]
     do
-      writeBold "Do you want to start the Kuzzle stack now? (y/N) "
+      promptBold "[❓] Do you want to start Kuzzle now? (y/N) "
       read launchTheStack trash
       case "$launchTheStack" in
         [yY])
+          echo
+          writeBold "Starting Kuzzle..."
           docker-compose -f $composerYMLPath up -d
-            echo
-            write "$GREEN" "Kuzzle is up and running!"
+          echo
+          write "$GREEN" "[✔] Kuzzle is up and running!"
           ;;
         [nN] | '')
           echo
-          echo "Great."
+          writeBold "$BLUE" "Ok."
+          launchTheStack=n
           ;;
         *)
           echo
-          echo "I did not understand your answer."
+          writeBold "$RED" "[✖] I did not understand your answer."
           ;;
       esac
     done
   echo
   writeBold "Where do we go from here?"
-  echo "* You can start your Kuzzle stack by typing:"
-  echo "  docker-compose -f $composerYMLPath up -d"
-  echo "* You can see the logs of your Kuzzle stack by typing:"
-  echo "  docker-compose -f $composerYMLPath logs -f"
-  echo "* You can check if everything is working by typing:"
-  echo "  curl -XGET http://localhost:7511/"
-  echo "* You can stop the Kuzzle stack by typing:"
-  echo "  docker-compose -f $composerYMLPath stop"
-  echo "* You can restart the Kuzzle stack by typing:"
-  echo "  docker-compose -f $composerYMLPath restart"
-  echo "* You can read the docs at http://docs.kuzzle.io/"
+  writeBold "* You can start Kuzzle by typing:"
+  write "  docker-compose -f $composerYMLPath up -d"
+  writeBold "* You can see the logs of the Kuzzle stack by typing:"
+  write "  docker-compose -f $composerYMLPath logs -f"
+  writeBold "* You can check if everything is working by typing:"
+  write "  curl -XGET http://localhost:7511/"
+  writeBold "* You can stop Kuzzle by typing:"
+  write "  docker-compose -f $composerYMLPath stop"
+  writeBold "* You can restart Kuzzle by typing:"
+  write "  docker-compose -f $composerYMLPath restart"
+  writeBold "* You can read the docs at http://docs.kuzzle.io/"
+  echo
 }
 
 # Main execution routine
 # ===========================
 
+echo
+writeBold "# Kuzzle Setup"
+writeBold "  ============"
+echo
+writeBold "This script will help you launch Kuzzle and install"
+writeBold "all the necessary dependencies."
+echo
+write     "* You can refer to http://docs.kuzzle.io/ if you need better"
+write     "  understanding of the installation process."
+write     "* Feel free to join us on Gitter at https://gitter.im/kuzzleio/kuzzle"
+write     "  to get help in real time."
+
 checkRoot
+# TODO check architecture (32 vs 64)
+# TODO check available memory
 findOSType
 isOSSupported
 
@@ -287,10 +314,11 @@ fi
 
 if ! $(docker run hello-world > /dev/null); then
   echo
-  echo "Docker does not seem to be running on your system."
-  echo "Please start the Docker daemon and re-run this script"
-  echo "More information at https://docs.docker.com/engine/admin/"
-  exit
+  writeBold "$RED" "[✖] Docker does not seem to be running on your system."
+  writeBold "$RED" "    Please start the Docker daemon and re-run this script"
+  write            "    More information at https://docs.docker.com/engine/admin/"
+  echo
+  exit 4
 fi
 
 if ! commandExists docker-compose; then
