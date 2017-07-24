a71095246f722eccc86c23824e2bf109c3c88b82
Luca Marchesini <luca@kuzzle.io>
2017-07-24 15:15:32 +0200

    Now it should fail

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kuzzle_host=http://localhost:7512/_healthcheck
+kuzzle_host=http://localhost:7512/_healthCheck
 timeout=${HEALTHCHECK_TIMEOUT:-60}
 
 docker-compose -f docker-compose/kuzzle-docker-compose.yml up -d
@@ -9,7 +9,7 @@ echo "[$(date --rfc-3339 seconds)] - Waiting for Kuzzle to be available"
 for i in `seq 1 $timeout`;
 do
     output=$( curl -s "$kuzzle_host" | grep \"status\":200 )
-    if [[ -z "$output" ]]; then
+    if [[ ! -z "$output" ]]; then
       echo "[$(date --rfc-3339 seconds)] - Kuzzle is online"
       exit 0
     fi
