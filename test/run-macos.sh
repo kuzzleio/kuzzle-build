795ff7d33f01ea81601ff0670d286f74884d8db6
Luca Marchesini <luca@kuzzle.io>
2018-05-17 17:53:00 +0200

    [MacOs] LOL etc/exports

--- test/run-macos.sh
+++ test/run-macos.sh
@@ -10,6 +10,7 @@ sudo chown root:wheel /usr/local/opt/docker-machine-driver-xhyve/bin/docker-mach
 sudo chmod u+s /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 
 curl --create-dirs -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v1.9.1/boot2docker.iso
+sudo rm /etc/exports
 docker-machine --github-api-token=$GITHUB_TOKEN create default --driver xhyve --xhyve-experimental-nfs-share
 
 sudo eval $(docker-machine env default)
