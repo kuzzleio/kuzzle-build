53552e23b11f4d1a128ebb2d8ec7053bb8f9233c
Luca Marchesini <luca@kuzzle.io>
2018-05-17 16:10:32 +0200

    MOAR MOAR

--- test/run-macos.sh
+++ test/run-macos.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-brew install expect docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
+brew install expect docker docker-compose docker-machine xhyve docker-machine-driver-xhyve || echo 'up to date'
 
 docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
 
 eval $(docker-machine env default)
+
+docker run -t hello-world
