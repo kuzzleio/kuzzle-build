61a75872971f2b5a8053e2ac2bce85d9a6a3025f
Luca Marchesini <luca@kuzzle.io>
2018-05-17 16:57:59 +0200

    [MacOS] Added chown

--- test/run-macos.sh
+++ test/run-macos.sh
@@ -4,6 +4,9 @@ brew update || echo 'up to date'
 
 brew install expect docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
 
+sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+
 sudo docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
 
 sudo eval $(docker-machine env default)
