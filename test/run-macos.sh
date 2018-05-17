dcac2b6bf9b1e9fb0d15588fb1ce5437765f0439
Luca Marchesini <luca@kuzzle.io>
2018-05-17 17:10:35 +0200

    [MacOS] Added set -x

--- test/run-macos.sh
+++ test/run-macos.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -x
+
 brew update || echo 'up to date'
 
 brew install expect docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
 
-sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chown root:wheel /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 
-sudo docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
+docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
 
 sudo eval $(docker-machine env default)
 
